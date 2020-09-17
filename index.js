a8471cbc10670f4d9f47b0abc2c45a49fc65567b
xaviguasch <xaviguasch@gmail.com>
2020-09-17 20:12:05 +0200

    added js logic and started styling

---
+++ index.js
@@ -0,0 +1,36 @@
+const totalBill = document.getElementById('totalBillAmount')
+const tipSelection = document.getElementById('tipPercentage')
+const numPeople = document.getElementById('numPeople')
+const calculateBtn = document.getElementById('calculateBtn')
+let billDisplay = document.getElementById('billDisplay')
+let tipDisplay = document.getElementById('tipDisplay')
+let cppDisplay = document.getElementById('cppDisplay')
+let partyTotal = document.getElementById('partyTotal')
+
+const displayResults = (
+  totalTip,
+  totalAmountToPay,
+  amountToPayPerPerson,
+  totalBillAmount
+) => {
+  billDisplay.textContent = `$${totalBillAmount.toFixed(2)}`
+  tipDisplay.textContent = `$${totalTip.toFixed(2)}`
+  cppDisplay.textContent = `$${amountToPayPerPerson.toFixed(2)}`
+  partyTotal.textContent = `$${totalAmountToPay.toFixed(2)}`
+}
+
+const calculateTip = (totalBillAmount, numberOfPeople, tipPercentage) => {
+  let totalTip = totalBillAmount * tipPercentage
+  let totalAmountToPay = totalBillAmount + totalTip
+  let amountToPayPerPerson = totalAmountToPay / numberOfPeople
+
+  displayResults(totalTip, totalAmountToPay, amountToPayPerPerson, totalBillAmount)
+}
+
+calculateBtn.addEventListener('click', () => {
+  const tipPercentage = parseFloat(tipSelection.selectedOptions[0].value)
+  const totalBillAmount = parseFloat(totalBill.value)
+  const numberOfPeople = parseFloat(numPeople.value)
+
+  calculateTip(totalBillAmount, numberOfPeople, tipPercentage)
+})
